f30ed3ae4791c3c154d9caad735bb9f3eecbb18e
jatharthan <jatharthan94@gmail.com>
2025-04-26 22:41:55 -0400

    web-tier

--- modules/ec2/outputs.tf
+++
@@ -1,9 +0,0 @@
-output "internal_app_lb_dns" {
-  description = "DNS of internal loadbalancer"
-  value       = aws_lb.internal_app_lb.dns_name
-}
-
-output "external_app_lb_dns" {
-  description = "DNS of external loadbalancer"
-  value       = aws_lb.external_web_lb.dns_name
-}
--- modules/iam/outputs.tf
+++
@@ -1,9 +0,0 @@
-output "iam_role_name" {
-  description = "IAM roles created"
-  value       = aws_iam_role.ec2_ssm_s3_role.name
-}
-
-output "aws_iam_instance_profile"{
-  description = "IAM instance profile"
-  value       = aws_iam_instance_profile.ec2_instance_profile.name
-}
