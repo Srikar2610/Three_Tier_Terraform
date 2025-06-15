101001bd9b449590983d68e222f9e9afed632cc5
Srikar2610 <srikarpadakanti10@gmail.com>
2025-06-15 19:24:56 +0530

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -1,9 +1,9 @@
 terraform {
   backend "s3" {
-    bucket         = "bootcamp1-tf-state-dev"
-    key            = "global/terraform.tfstate"
+    bucket         = "srikar-terraform-backend"
+    key            = "3tier-infra/terraform.tfstate"
     region         = "us-east-1"
-    dynamodb_table = "bootcamp1-terraform-lock-table-dev"
+    dynamodb_table = "terraform-locks"
     encrypt        = true
   }
 }
